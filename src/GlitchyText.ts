2e35f10922fec6ce175b1467524c3322e555fb7e
Nicholas Pisacane <pisacanen@gmail.com>
2019-01-07 17:54:10 -0800

    add helper for glitchy text stuff

--- src/GlitchyText.ts
+++ src/GlitchyText.ts
@@ -1,19 +1,26 @@
 const GLITCHY_CLASS_PFX = `glitchy-text`;
 
+const DEFAULT_COLORS = ['#ff196a', '#3fff19', '#2c19ff'];
+
 export default class GlitchyText {
   private colors: string[] = [];
   private interval: number = -1;
-  private delay: number = -1;
+  private delay: number = 0;
   private container: Element;
 
   public static createFromElement(element: Element): GlitchyText {
-    const colors = element.getAttribute('data-glitchy-colors').split(' ');
+    const colors = element.hasAttribute('data-glitchy-colors')
+      ? element.getAttribute('data-glitchy-colors').split(' ')
+      : DEFAULT_COLORS;
     let interval = -1;
+    let delay = 0;
     if (element.hasAttribute('data-glitchy-interval')) {
       interval = parseInt(element.getAttribute('data-glitchy-interval'), 10);
     }
-
-    return new GlitchyText(element, colors, interval);
+    if (element.hasAttribute('data-glitchy-delay')) {
+      delay = parseInt(element.getAttribute('data-glitchy-delay'), 10);
+    }
+    return new GlitchyText(element, colors, interval, delay);
   }
 
   public static initializeAll() {
@@ -22,40 +29,52 @@ export default class GlitchyText {
     });
   }
 
-  constructor(container: Element, colors: string[], interval: number = -1) {
+  constructor(
+    container: Element,
+    colors: string[],
+    interval: number = -1,
+    delay: number = 0,
+  ) {
     this.container = container;
     this.colors = colors;
     this.interval = interval;
+    this.delay = delay;
 
     this.initialize();
   }
 
   private initialize() {
     const content = this.container.innerHTML;
+    const className = this.container.getAttribute('data-glitchy-class') || '';
     const original = document.createElement('div');
-    original.className = `${GLITCHY_CLASS_PFX}__original`;
+
+    original.className = `${GLITCHY_CLASS_PFX}__original ${className}`;
     original.innerHTML = content;
     this.container.innerHTML = '';
     this.container.appendChild(original);
 
     this.colors.forEach((color, idx) => {
       const layer = document.createElement('div');
-      layer.className = `${GLITCHY_CLASS_PFX}__layer`;
+      layer.className = `${GLITCHY_CLASS_PFX}__layer ${className}`;
       layer.innerHTML = content;
       layer.style.color = color;
       this.container.insertBefore(layer, original);
 
       if (this.interval > 0) {
-        let animation = `glitch-text-${idx} 500ms linear ${this.interval}ms`;
-        if (this.delay > 0) {
-          animation += ` ${this.delay}ms`;
-        }
-        layer.style.animation = animation;
+        setTimeout(() => {
+          setInterval(() => {
+            layer.style.animation = `glitchy-text-${idx + 1} 500ms linear`;
+          }, this.interval);
+        }, this.delay);
+
+        layer.addEventListener(
+          'animationend',
+          () => {
+            layer.style.animation = '';
+          },
+          false,
+        );
       }
     });
-
-    if (this.interval > 0) {
-      // TODO
-    }
   }
 }
